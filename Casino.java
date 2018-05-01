920e0089dfce2d84946c016e836ecd46a8345d09
DynamiteDave <davedlawrence1@gmail.com>
2018-05-01 08:57:04 -0700

    Update Casino.java

--- Casino.java
+++ Casino.java
@@ -1,1 +1,20 @@
-
+public class Casino
+{
+private double money;
+  
+  public Casino()
+  {
+    money =1000;
+  }
+  
+  public double getMoney()
+  {
+    return money();
+  }
+  
+  public void changeMoney(double cash)
+  {
+    money = cash;
+  }
+  
+}
