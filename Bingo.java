48b2de2829e41d8eba4c7905b08a2185ff906e6a
DynamiteDave <davedlawrence1@gmail.com>
2018-05-01 09:10:26 -0700

    Update Bingo.java

--- Bingo.java
+++ Bingo.java
@@ -4,6 +4,20 @@ String[][] board;
 
 public Bingo()
 {
-board = new String[6][6]();
+  board = new String[5][5]();
 }
+  
+public void drawBoard()
+{
+  for (int col = 1; col <= 5; col++)
+  {
+    for (int row = 0; row < 5; row++)
+    {
+      board[row][col -1] = (int)Math.random() * 20 * col;
+    }
+  }
+  board[2][2] = "o";
+}
+  
+  
 }
