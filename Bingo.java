c1a88962cdec6d0fc34b586185a386dabc5d6617
DynamiteDave <davedlawrence1@gmail.com>
2018-05-01 09:45:03 -0700

    Update Bingo.java

--- Bingo.java
+++ Bingo.java
@@ -1,10 +1,10 @@
 public class Bingo
 {
-String[][] board;
+int[][] board;
 
 public Bingo()
 {
-  board = new String[5][5]();
+  board = new int[5][5];
 }
   
 public void drawBoard()
@@ -16,8 +16,73 @@ public void drawBoard()
       board[row][col -1] = (int)Math.random() * 20 * col;
     }
   }
-  board[2][2] = "o";
+  board[2][2] = 0;
 }
+  
+  public boolean increment()
+  {
+    int rand = (int)Math.random() *100;
+    for (int col = 1; col <= 5; col++)
+     {
+    for (int row = 0; row < 5; row++)
+    {
+      if( board[row][col-1] == rand)
+      {
+        board[row][col-1] = 0;
+      }
+    }
+  }
+    
+    
+    int check 0;
+    for( int row = 0; row < 5; row ++)
+    {
+      for (int col = 0; col < 5; col ++)
+      {
+        if( board[row][col] = 0)
+        {
+          check++
+        }
+        if ( check == 5)
+        {
+          return true
+        }
+      }
+      check =0;
+    }
+    
+    for (int col = 0; col < 5; col ++)
+    {
+      for (int row = 0; row < 5; row++)
+      {
+        if( board[row][col] = 0)
+        {
+          check++
+        }
+        if ( check == 5)
+        {
+          return true
+        }
+      }
+      check =0;
+    }
+    
+    for (int both = 0; both < 5; both++)
+    {
+      if ( board[both][both] = 0)
+      {
+        check++;
+      }
+      if(check == 5)
+      {
+        return true;
+      }
+    }
+    
+    
+    
+    
+  }
   
   
 }
