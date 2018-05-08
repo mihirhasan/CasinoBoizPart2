cd22c5b4cb7ea462fa7b456129a982cbd36fc846
mihir1357 <38732914+mihir1357@users.noreply.github.com>
2018-05-08 08:33:57 -0700

    Create Card.java

---
+++ Card.java
@@ -0,0 +1,31 @@
+package test;
+
+public class Card
+{
+    String nam;
+
+    int value;
+
+
+    public Card( String name, int val )
+    {
+        nam = name;
+        value = val;
+    }
+
+
+    public int getVal()
+    {
+        return value;
+    }
+
+
+    public String getName()
+    {
+        return nam;
+    }
+    public void setAce(int num)
+    {
+        value = num;
+    }
+}
