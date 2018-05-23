936577fb3f4ece179e2fe90cbda98f6919d9df98
mihir1357 <38732914+mihir1357@users.noreply.github.com>
2018-05-23 10:33:11 -0700

    Update SlotMachine.java

--- SlotMachine.java
+++ SlotMachine.java
@@ -3,19 +3,28 @@ package test;
 public class SlotMachine
 {
     Casino casi;
-
-
-    public SlotMachine()
-    {
-        casi = new Casino();
-    }
-
     String[] icons = { "Kumquat", "Diamond", "Cherries", "Melon", "Heart", "Penguin", "Bananas",
         "Horseshoe", "Dog", "Fulk" };
 
-    String[] results = new String[3];
+    String[] results;
+    
+    /**
+     * Constructor gives the player 100 coins to begin with and also intiializes the results
+     * array that will eventually hold the icons received.
+     */
+    public SlotMachine()
+    {
+        casi = new Casino(100);
+        results = new String[3];;
+    }
 
 
+    /**
+     * 
+     * codes functionality behind the spin button. Chooses three random icons when spin 
+     * button is clicked. Calls payout method to decide how much the winnings are for 
+     * that unique icon combination
+     */
     public void spin()
     {
         casi.removeMoney( 10 );
@@ -27,12 +36,25 @@ public class SlotMachine
         results[1] = icons[rand1];
         results[2] = icons[rand2];
         payout();
-        display();
     }
 
     int toAdd = 0;
-
-
+    /**
+     * 
+     * accesses the results array to be used in window/testing
+     * @return results array
+     */
+    public String[] getResult()
+    {
+        return results;
+    }
+    /**
+     * 
+     * sets the winnings for each combination. The Casino operates on a 90% back system. 
+     * E.g if the user spends 100 dollars, he or she theoretically receives 90 dollars back.
+     * The probabilities of receiving each "jackpot" are set to this same ratio. 
+     * The largest jackpot is Fulk Fulk Fulk. Go Mr. Fulk!
+     */
     public void payout()
     {
         // 90% money back ratio
@@ -63,21 +85,32 @@ public class SlotMachine
         }
         casi.addMoney( toAdd );
     }
-
-
-    private void display()
+    /**
+     * 
+     * accesses the amount of money that is paid back in the payout method to be used 
+     * for display methods.
+     * @return toAdd. Integer of coins to be added when payout is called.
+     */
+    public int getPay()
     {
-        System.out.println( results[0] + " || " + results[1] + " || " + results[2] );
-        if ( toAdd > 0 )
-        {
-            System.out.println( "Congratulations! You made " + toAdd + " buckeroos!" );
-        }
-        else
-        {
-            System.out.println( "Sorry, you LOSE" );
-        }
-        System.out.println( "Current Balance:" + casi.getMoney() + "" );
-
+        return toAdd;
+    }
+    /**
+     * 
+     * gets the total money the user has left
+     * @return the balance of the user
+     */
+    public int getTotal()
+    {
+        return casi.getMoney();
+    }
+    /**
+     * 
+     * sets money back to original amount when users restart the game
+     */
+    public void reset()
+    {
+        casi.setMoney( 100 );
     }
 
 }
