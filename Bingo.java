764d7d46a796e8bde582c4c2c370fb22434cece4
DynamiteDave <davedlawrence1@gmail.com>
2018-05-30 19:50:31 -0700

    Update Bingo.java

--- Bingo.java
+++ Bingo.java
@@ -1,6 +1,18 @@
+package src;
+
 import java.util.Random;
 
 
+/**
+ *  The bingo game for our casino class. The user can play this game via the casino windo. 
+ *
+ *  @author  Dave Desouza-Lawrence
+ *  @version May 30, 2018
+ *  @author  Period: 2
+ *  @author  Assignment: CasiYES
+ *
+ *  @author  Sources: None
+ */
 public class Bingo
 {
     public int[][] board;
@@ -8,6 +20,9 @@ public class Bingo
     Random rand;
 
 
+    /**
+     * Standard bingo constructor. 
+     */
     public Bingo()
     {
         board = new int[5][5];
@@ -15,22 +30,28 @@ public class Bingo
     }
 
 
+    /**
+     * Fills the board with random values.
+     */
     public void drawBoard()
     {
         for ( int row = 0; row < 5; row++ )
         {
             for ( int col = 1; col <= 5; col++ )
             {
-                board[row][col - 1] = rand.nextInt(  20  ) + 20*row;
+                board[row][col - 1] = rand.nextInt(  20  ) + 20 * row + 1;
             }
         }
         board[2][2] = 0;
     }
 
 
+    /**
+     * Adds an element to the board and check if should be replaced.
+     * @return
+     */
     public int increment()
-    {
-        
+    {  
             int random = (rand.nextInt(100)) + 1;
             for ( int col = 1; col <= 5; col++ )
             {
@@ -46,6 +67,10 @@ public class Bingo
     }
 
 
+    /**
+     * Checks if there has been a win in the board
+     * @return true if won or false if lost
+     */
     public boolean check()
     {
         int check = 0;
@@ -113,11 +138,28 @@ public class Bingo
     }
 
 
+    /**
+     * Returns the board
+     * @return the board
+     */
     public int[][] myBoard()
     {
         return board;
     }
     
+    /**
+     * Sets teh board to a array. Used only for testing
+     * @param array the array 
+     */
+    public void setBoard( int[][] array)
+    {
+        board = array;
+    }
+    
+    /**
+     * Returns teh random object
+     * @return the random object
+     */
     public Random getRand()
     {
         return rand;
